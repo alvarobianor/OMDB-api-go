e9061dbac10283d03be540915ad8582799f11e80
Álvaro Bianor <bianor.alvaro12@gmail.com>
2025-04-30 07:45:01 -0300

    Implement movie retrieval API with error handling and JSON response formatting

--- api/handler.go
+++ api/handler.go
@@ -1,11 +1,22 @@
 package api
 
 import (
+	"encoding/json"
+	"fmt"
+	"log/slog"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
 )
+
+type Response struct {
+	Error string      `json:"error,omitempty"`
+	Data  interface{} `json:"data,omitempty"`
+}
+
+const apiUrl = "https://www.omdbapi.com/?s=%s&apikey=%s"
 
 func Handler(apiKey string) http.Handler {
 	router := chi.NewMux()
@@ -20,11 +31,79 @@ func Handler(apiKey string) http.Handler {
 	})
 
 	router.Use(corsMiddleware.Handler)
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
 
-	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("Hello, World!"))
+	router.Use(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			r.Header.Set("X-CUSTOM-HEADER-API-KEY", apiKey)
+			w.Header().Set("Content-Type", "application/json")
+			next.ServeHTTP(w, r)
+		})
 	})
 
+	router.Get("/{movie}", getMovie())
+
 	return router
+}
+
+func getMovie() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		apiKeyHeader := r.Header.Get("X-CUSTOM-HEADER-API-KEY")
+		if apiKeyHeader == "" {
+			errMessage := "X-CUSTOM-HEADER-API-KEY is not set"
+			slog.Error("Unauthorized", "error", errMessage)
+			SendJson(w, Response{Error: errMessage}, http.StatusUnauthorized)
+			return
+		}
+
+		movieName := chi.URLParam(r, "movie")
+
+		if movieName == "" {
+			errMessage := "Movie name is not set"
+			slog.Error("Bad Request", "error", errMessage)
+			SendJson(w, Response{Error: errMessage}, http.StatusBadRequest)
+			return
+		}
+
+		concatUrlApi := fmt.Sprintf(apiUrl, movieName, apiKeyHeader)
+
+		resp, err := http.Get(concatUrlApi)
+		if err != nil {
+			errMessage := "Failed to get movie"
+			slog.Error("Failed to get movie", "error", err)
+			SendJson(w, Response{Error: errMessage}, http.StatusInternalServerError)
+			return
+		}
+
+		var data any
+
+		if errDecode := json.NewDecoder(resp.Body).Decode(&data); errDecode != nil {
+			errMessage := "Failed to decode movie"
+			slog.Error("Failed to decode movie", "error", errDecode)
+			SendJson(w, Response{Error: errMessage}, http.StatusInternalServerError)
+			return
+		}
+
+		SendJson(w, Response{Data: data}, http.StatusOK)
+	}
+}
+
+func SendJson(w http.ResponseWriter, r Response, code int) {
+	data, err := json.Marshal(r)
+
+	if err != nil {
+		slog.Error(err.Error(), "error", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(code)
+	_, err = w.Write(data)
+
+	if err != nil {
+		slog.Error(err.Error(), "error", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
